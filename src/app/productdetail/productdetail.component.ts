89ea9b05c33ffca9fff5d9ccbbf01e1a67760a1a
viral <viralondhiya@gmail.com>
2020-08-27 17:26:01 +0530

    update 25

--- src/app/productdetail/productdetail.component.ts
+++ src/app/productdetail/productdetail.component.ts
@@ -14,54 +14,60 @@ export class ProductdetailComponent implements OnInit {
   productName: any;
   productList: any[] = []
   selectedProduct: any;
-    selectedImages: any[];
-    selectedSizes: any[];
-    selectchangeImage: any;
-    selectedchangeProduct: any;
-  constructor(private router: Router, 
-    private _activatedRoute: ActivatedRoute, 
+  selectedImages: any[];
+  selectedSizes: any[];
+  selectchangeImage: any;
+  selectedchangeProduct: any;
+  constructor(private router: Router,
+    private _activatedRoute: ActivatedRoute,
     private productDetailService: ProductDetailService,
-    public share: ShareButtons,) {
+    public share: ShareButtons, ) {
     this._activatedRoute.params.subscribe((params: any) => {
-      
-      
-       this.productName = params['productName']; 
-       //console.log('this.productName',this.productName);
-       if (this.productName) this.getProductDetailbyProName();
 
-   })
 
-   }
+      this.productName = params['productName'];
+      //console.log('this.productName',this.productName);
+      if (this.productName) this.getProductDetailbyProName();
+
+    })
+
+  }
 
   ngOnInit(): void {
   }
-getProductDetailbyProName(){
-  this.productDetailService.productDetailbyProName(this.productName).subscribe((data: any) => {
-    this.productList = []
+  getProductDetailbyProName() {
+    this.productDetailService.productDetailbyProName(this.productName).subscribe((data: any) => {
+      this.productList = []
 
-   data.map(e => {
-       var dt: any = e.payload.doc.data();
-       this.productList.push({
-           id: e.payload.doc.id, 
-           product_name: dt.product_name, 
-           price: dt.price,
-           material: dt.material, 
-           ProURL: dt.ProURL, 
-           ProPath: dt.ProPath,
+      data.map(e => {
+        var dt: any = e.payload.doc.data();
+        this.productList.push({
+          id: e.payload.doc.id,
+          product_name: dt.product_name,
+          price: dt.price,
+          material: dt.material,
+          ProURL: dt.ProURL,         
+          ProPath: dt.ProPath,
           //  proImages: dt.proImages,
           //  gender: dt.gender.id,
           //  scategoryID: dt.name.id,
-           name: dt.name,
-           size: dt.size
-       })
-  
-   });
- 
+          name: dt.name,
+          size: dt.size
+        })
+      });
+      console.log(this.productList)
+      //console.log('this.selectedImages', this.selectedImages)
+    })
 
-   console.log(this.productList)
+
+  }
+
+  // selectProduct(event, product) {
+  //   this.selectedProduct = ''
+
+  //   this.selectedProduct = product
+  //   this.selectedImages = this.selectedProduct?.ProURL
+
    
-})
-
-
-}
+  // }
 }
